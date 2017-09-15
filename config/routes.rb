24bb7f7876100313483d574746cdf0ad77977a3e
Rob <rob.edwards101@googlemail.com>
2017-09-15 13:48:06 +0100

    Added signup route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'login', to: 'user_sessions#new'
   get 'logout', to: 'user_sessions#destroy'
+  get 'signup', to: 'users#new'
 
   resources :user_sessions, only: [ :new, :create, :destroy ]
   scope :admin do
-    resources :users
+    resources :users, except: [ :new ]
   end
 end
