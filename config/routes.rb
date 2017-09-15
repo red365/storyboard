bba3d302df2b2ebdb214860c7bb09e9045ec1f30
Rob <rob.edwards101@googlemail.com>
2017-09-15 01:11:39 +0100

    Add create and destroy session functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: 'user_sessions#new'
+  get 'logout', to: 'user_sessions#destroy'
+
+  resources :user_sessions, only: [ :new, :create, :destroy ]
   scope :admin do
     resources :users
   end
