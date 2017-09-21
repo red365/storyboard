9f9975a3480027786a001a99daa8dbbb05ba2d46
Rob <rob.edwards101@googlemail.com>
2017-09-21 13:09:28 +0100

    Added Project scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'login', to: 'user_sessions#new'
   get 'logout', to: 'user_sessions#destroy'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915125232) do
+ActiveRecord::Schema.define(version: 20170921120650) do
 
   create_table "organisations", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
