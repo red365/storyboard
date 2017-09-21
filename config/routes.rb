e552be955488ba818522c649671f11eee6893d85
Rob <rob.edwards101@googlemail.com>
2017-09-21 20:06:25 +0100

    Added a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/', to: 'home#index'
+  
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'login', to: 'user_sessions#new'
