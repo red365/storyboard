a218c411e94476dcf5eb82d43d9007f7500b40ad
Rob <rob.edwards101@googlemail.com>
2017-09-24 13:55:42 +0100

    display domain list on project show page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :users, except: :new
   end
   resources :projects do
-    resources :domains
+    resources :domains, except: [ :index ]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
